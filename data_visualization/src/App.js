33a0ad11d33e07970d5495680bb8f50818a77719
MingHao Juan <howardjuan0118@gmail.com>
2019-10-19 13:04:38 +0800

    Heatmap done

--- data_visualization/src/App.js
+++ data_visualization/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MapContainer from "./MapContainer";
-import axios from 'axios';
 import SidebarProfile from './sidebar';
 import Example from './Popover';
 import People from './people';
 import './App.css';
+import apiKey from './api_key.txt';
+import cityData from './city_data.json';
 
 
+const aqiScale = [
+  50, 100, 150, 200, 300, 1000
+];
 
 class App extends React.Component{
 
@@ -14,8 +18,10 @@ class App extends React.Component{
     super(props);
     // initialize our state
     this.state = {
-    data: [],
-    peopleClicked: false
+      data: [],
+      peopleClicked: false,
+      cityData: {},
+      apiKey: undefined
   	};
   	this.onSetPeopleClicked = this.onSetPeopleClicked.bind(this);
     this.onClickPeople = this.onClickPeople.bind(this);
@@ -31,27 +37,41 @@ class App extends React.Component{
     this.onSetPeopleClicked(!this.state.peopleClicked);
   }
 
-  // componentDidMount(){
-  // 	this.getDataFromDb();
-  // };
-
-  getDataFromDb = () => {
-    fetch('http://localhost:3001/api/getData')
-      .then((data) => data.json())
-      .then((res) => console.log(res.data));
+  catagorizeAqi = () => {
+    for (const [key, value] of Object.entries(cityData)) {
+      if (cityData.hasOwnProperty(key) && value.city.geo !== null) {
+        for (const [idx, aqi] of aqiScale.entries()) {
+          if (value.aqi <= aqi) {
+            cityData[key].aqi = idx + 1;
+            break;
+          }
+        }
+      }
+    }
+    this.setState({cityData: cityData});
   };
 
+  componentDidMount(){
+    // fetch(cityData)
+    //   .then(data => data.json())
+    //   .then(res => this.setState({ data: res.data }));
+    this.catagorizeAqi();
 
+    fetch(apiKey)
+      .then(data => data.text())
+      .then(res => this.setState({apiKey: res}));
+  };
 
   render() {
-  	console.log(this.state.data);
-  	return(
-  		<div>
-        <MapContainer />
+  	return (
+      <div>
+        {this.state.apiKey === undefined ? null :
+          <MapContainer apiKey={this.state.apiKey} cityData={this.state.cityData} />
+        }
         {this.state.peopleClicked ? <People />:<div></div>}
         <SidebarProfile onClickPeople={this.onClickPeople.bind(this)}/>
-        </div>
-        )
+      </div>
+    );
   }
 }
 
